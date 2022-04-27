ddbb01d123e9a966aba99d22c6e17ecff19a9b9c
Sergey Byvshev <byvshevsp@m2.ru>
2022-04-27 23:12:11 +0300

    init
    # Conflicts:
    #	README.md

---
+++ api/v1beta/agent_types.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+type Limit struct {
+	Cpu    string `json:"cpu,omitempty"`
+	Memory string `json:"memory,omitempty"`
+}
+
+type Requests struct {
+	Cpu    string `json:"cpu,omitempty"`
+	Memory string `json:"memory,omitempty"`
+}
+
+type Resources struct {
+	Limit    Limit    `json:"limits,omitempty"`
+	Requests Requests `json:"requests,omitempty"`
+}
+
+// AgentSpec defines the desired state of M2Logstash
+type AgentSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of M2Logstash. Edit M2Logstash_types.go to remove/update
+	Image                 string    `json:"image,omitempty"`
+	Tag                   string    `json:"tag,omitempty"`
+	Name                  string    `json:"name,omitempty"`
+	MetricsScrapeInterval int       `json:"metricsScrapteInterval,omitempty"`
+	InternalLogs          bool      `json:",omitempty"`
+	Resources             Resources `json:"resources,omitempty"`
+}
+
+// +patchMergeKey=type
+// +patchStrategy=merge
+// +listType=map
+// +listMapKey=type
+// M2LogstashStatus defines the observed state of Agent
+
+type AgentStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Agent is the Schema for the agents API
+type Agent struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AgentSpec   `json:"spec,omitempty"`
+	Status AgentStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// M2LogstashList contains a list of M2Logstash
+type AgentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Agent `json:"items"`
+}
